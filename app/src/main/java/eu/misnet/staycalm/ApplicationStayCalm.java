632f14b733bbcef3cae4279dfab3875b585539f6
Ryczypisk <michal.chojaczyk@gmail.com>
2015-09-10 14:48:16 +0200

    link to publication in google play

--- app/src/main/java/eu/misnet/staycalm/ApplicationStayCalm.java
+++ app/src/main/java/eu/misnet/staycalm/ApplicationStayCalm.java
@@ -7,6 +7,7 @@ import android.view.MenuItem;
 /**
  * Created by MChojaczyk on 2015-09-08.
  * ToDo:
+ * 0. publikacja: https://play.google.com/apps/publish/?dev_acc=10320453111401279897#ContentRatingPlace:p=tmp.10320453111401279897.1441724885135
  * 1. Swipowanie pomiędzy podpowiedziami http://developer.android.com/training/implementing-navigation/lateral.html#horizontal-paging
  * 2. Przypomnienia raz dziennie + ustawienia wł/wył
  * 3. ustawienia kiedy przypomnienia
