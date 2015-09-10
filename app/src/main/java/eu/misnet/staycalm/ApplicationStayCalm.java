384f17cba2c517d06c9254d5dd517131630167b9
Ryczypisk <michal.chojaczyk@gmail.com>
2015-09-10 12:59:41 +0200

    added to do list

--- app/src/main/java/eu/misnet/staycalm/ApplicationStayCalm.java
+++ app/src/main/java/eu/misnet/staycalm/ApplicationStayCalm.java
@@ -6,6 +6,10 @@ import android.view.MenuItem;
 
 /**
  * Created by MChojaczyk on 2015-09-08.
+ * ToDo:
+ * 1. Swipowanie pomiędzy podpowiedziami http://developer.android.com/training/implementing-navigation/lateral.html#horizontal-paging
+ * 2. Przypomnienia raz dziennie + ustawienia wł/wył
+ * 3. ustawienia kiedy przypomnienia
  */
 public class ApplicationStayCalm extends Application {
 
